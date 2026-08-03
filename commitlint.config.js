b698aa618779f0eb9c15b231349e4e10630ac3b5
Pramod <pramod.narayana@gmail.com>
2026-08-03 18:03:10 +0530

    fix: inbound and outbound edi flow

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,5 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  helpUrl:
+    "\n\n💡 FORMAT EXPECTED: <type>(<scope>): <subject>\n👉 EXAMPLE: fix: added inbound EDI flow\n⚠️  Don't forget the SPACE after the colon!\n\nRead more at: https://www.conventionalcommits.org/en/v1.0.0/",
+};
